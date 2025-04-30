45e4161a34df85c7d449346e735cf678279504e9
KarolaineAlencar <kalencar98@gmail.com>
2025-04-29 21:23:29 -0300

    commit inicial.

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+// Component Pages
+import { HomeComponent } from './modules/porfolio/pages/home/home.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    }
+];
